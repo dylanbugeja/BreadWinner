5fb3632d50e77bae6122c758f3073e375c9a3af4
dylanbugeja <dylan.j.bugeja@gmail.com>
2021-01-27 06:36:41 -0500

    Removed Mirror and Added Input Controls
    I removed Mirror from the files, In addition i set up a test scene with 2 players in it that can be controlled by different inputs as soon as a button is pressed on the input

---
+++ Bread Winner/Assets/Scripts/Player Scripts/PlayerInputHandler.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class PlayerInputHandler : MonoBehaviour
+{
+
+    private PlayerMovement mover;
+    private PlayerInput playerInput;
+    private void Awake()
+    {  
+        playerInput = GetComponent<PlayerInput>();
+        var index = playerInput.playerIndex;
+        var movers = FindObjectsOfType<PlayerMovement>();
+        mover = movers.FirstOrDefault(m => m.GetPlayerIndex() == index);
+    }
+    public void OnMove(InputAction.CallbackContext context)
+    {
+        if (mover != null)
+        mover.SetInputValue(context.ReadValue<Vector2>());
+    }
+    public void OnJump(InputAction.CallbackContext context)
+    {
+        if (mover != null)
+        mover.Jump(context.performed);
+    }
+
+}
---
+++ Bread Winner/Assets/Scripts/Player Scripts/PlayerMovement.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    private float inputX;
+    private Collider2D col;
+
+    [SerializeField] private LayerMask ground;
+    [SerializeField] private float moveSpeed = 4.0f;
+    [SerializeField] private float jumpForce = 5.0f;
+    [SerializeField] private Rigidbody2D rb;
+    [SerializeField] private int playerIndex = 0;
+    private void Awake()
+    {
+        col = GetComponent<Collider2D>();
+    }
+    void Update()
+    {
+        Move();
+    }
+    public int GetPlayerIndex()
+    {
+        return playerIndex;
+    }
+    public void SetInputValue(Vector2 inputValue)
+    {
+        inputX = inputValue.x;
+    }
+    private void Move()
+    {
+       rb.velocity = new Vector2(inputX * moveSpeed, rb.velocity.y);
+    }
+    public void Jump(bool performed)
+    {
+        if (performed && IsGrounded())
+        {
+            rb.velocity = new Vector2(rb.velocity.x, jumpForce);
+        }
+    }
+    private bool IsGrounded()
+    {
+        Vector2 tlPoint = transform.position;
+        tlPoint.x -= col.bounds.extents.x;
+        tlPoint.y += col.bounds.extents.y;
+
+        Vector2 brPoint = transform.position;
+        brPoint.x += col.bounds.extents.x;
+        brPoint.y -= col.bounds.extents.y;
+
+        return Physics2D.OverlapArea(tlPoint, brPoint, ground);
+    }
+}
