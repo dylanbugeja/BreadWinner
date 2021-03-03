814e9f59886e8890c9e07819da7950a3eaaaa50f
dylanbugeja <dylan.j.bugeja@gmail.com>
2021-03-03 06:36:40 -0500

    Art and Hotfixes
    I added the Animations and options for Bagel, Loaf and Baguette so they can now be played when selected from the player select screen. Merged some of the obstacles and buffs from Soo's project. Still need to add game manager and do adjustments to the camera and add Xingwei's end screen

---
+++ Bread Winner/Assets/Scripts/LevelDestroyer.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LevelDestroyer : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        StartCoroutine("DestroySelf");
+    }
+    IEnumerator DestroySelf()
+    {
+        Destroy(this);
+        yield return new WaitForSecondsRealtime(25f);
+    }
+
+}
