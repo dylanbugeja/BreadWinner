102e828cdc831c7ae83aa8a8dce72bf0eb4c717e
dylanbugeja <dylan.j.bugeja@gmail.com>
2021-03-15 00:49:24 -0400

    update

--- Bread Winner/Assets/Scripts/LevelDestroyer.cs
+++ Bread Winner/Assets/Scripts/LevelDestroyer.cs
@@ -7,12 +7,12 @@ public class LevelDestroyer : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        StartCoroutine("DestroySelf");
+        StartCoroutine("DestroySelf()");
     }
     IEnumerator DestroySelf()
     {
-        Destroy(this);
-        yield return new WaitForSecondsRealtime(25f);
+        Destroy(gameObject);
+        yield return new WaitForSeconds(60f);
     }
 
 }
